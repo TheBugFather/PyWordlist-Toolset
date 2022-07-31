3fbf522761c5e7b812c9dccdc8b7fc663e344732
Nicholas Gimbel <56981761+ngimb64@users.noreply.github.com>
2022-07-31 01:11:48 -0400

    Update - 07/31/22

--- WordlistFileParser.py
+++ WordlistFileParser.py
@@ -1,4 +1,5 @@
 # Built-in modules #
+import os
 import re
 import sys
 
@@ -10,25 +11,42 @@ from Modules.Utils import ErrorQuery, PrintErr
 ########################################################################################################################
 Name:       main
 Purpose:    Iterates through input file, matching words, and creating wordlist from results.
-Parameters: None
-Returns:    None
+Parameters: Nothing
+Returns:    Nothing
 ########################################################################################################################
 """
 def main():
+    # Get the working directory #
+    cwd = os.getcwd()
+
+    # If the OS is Windows #
+    if os.name == 'nt':
+        path = f'{cwd}\\'
+    # If the OS is Linux #
+    else:
+        path = f'{cwd}/'
+
     # If a file name arg was passed in #
     if len(sys.argv) > 1:
-        filename = f'./{sys.argv[1]}'
+        filename = f'{path}{sys.argv[1]}'
+
+        # If the arg file name does not exist #
+        if not os.path.isfile(filename):
+            PrintErr('Passed in arg file name does not exist')
+            sys.exit(1)
+    # If user failed to provide input file name #
     else:
-        PrintErr('No name of file to be parsed provided as arg .. try again with \"Text_Parser.py <file name>\"')
+        PrintErr('No name of file to be parsed provided .. try again with \"WordlistFileParser.py <file name>\"')
         sys.exit(1)
 
     # Only record unique strings #
     string_set = set()
     # Compile string matching regex #
-    re_string = re.compile(r'[a-zA-Z]{3,15}(?: |$)')
+    re_string = re.compile(r'[a-zA-Z\d!@$&(\-_\"\'.,]{4,15}(?: |$)')
 
+    mode = 'r'
     try:
-        with open(filename, 'r') as file:
+        with open(filename, mode) as file:
             for line in file:
                 # Check line of text for matches, populate into list #
                 string_parse = re.findall(re_string, line)
@@ -42,13 +60,9 @@ def main():
                     # Write results to report file #
                     [string_set.add(string) for string in string_parse]
 
-    # If file error occurs #
-    except (IOError, OSError) as err:
-        ErrorQuery(filename, 'r', err)
-
-    try:
+        mode = 'a'
         # Open the wordlist in append mode #
-        with open('wordlist.txt', 'a') as report_file:
+        with open('wordlist.txt', mode) as report_file:
             for string in string_set:
                 # Remove extra whitespace #
                 parse = string.strip()
@@ -57,7 +71,9 @@ def main():
 
     # If file error occurs #
     except (IOError, OSError) as err:
-        ErrorQuery('wordlist.txt', 'a', err)
+        PrintErr(f'Error occurred during file operation: {err}')
+        ErrorQuery('wordlist.txt', mode, err)
+        sys.exit(2)
 
     sys.exit(0)
 
--- WordlistSanitizer.py
+++ WordlistSanitizer.py
@@ -1,4 +1,5 @@
 # Built-in modules #
+import os
 import sys
 
 # Custom modules #
@@ -8,44 +9,60 @@ from Modules.Utils import ErrorQuery, PrintErr
 """
 ########################################################################################################################
 Name:       main
-Purpose:    Iterates through input file, matching words, and creating wordlist from results.
-Parameters: None
-Returns:    None
+Purpose:    Iterates through input wordlist, stripping any extra whitespace outside of each item and overwriting the \
+            results to a fresh sanitized wordlist.
+Parameters: Nothing
+Returns:    Nothing
 ########################################################################################################################
 """
 def main():
+    # Get the working directory #
+    cwd = os.getcwd()
+
+    # If the OS is Windows #
+    if os.name == 'nt':
+        path = f'{cwd}\\'
+    # If the OS is Linux #
+    else:
+        path = f'{cwd}/'
+
     # If a file name arg was passed in #
     if len(sys.argv) > 1:
-        filename = f'./{sys.argv[1]}'
+        filename = f'{path}{sys.argv[1]}'
+
+        # If the arg file name does not exist #
+        if not os.path.isfile(filename):
+            PrintErr('Passed in arg file name does not exist')
+            sys.exit(1)
+    # If user failed to provide input wordlist name #
     else:
-        PrintErr('No name of wordlist file provided .. try again')
+        PrintErr('No name of wordlist file provided .. try again with \"WordlistSanitizer <wordlist name>\"')
         sys.exit(1)
 
     # Filter set with only unique strings #
     parse_set = set()
 
+    mode = 'r'
     try:
         # Open current wordlist in read mode #
-        with open(filename, 'r') as file:
+        with open(filename, mode) as file:
             for line in file:
                 # Strip out unnecessary whitespace #
                 parsed_text = line.strip()
                 # Add parsed text to set #
                 parse_set.add(parsed_text)
 
-    # If file error occurs #
-    except (IOError, OSError) as err:
-        ErrorQuery(filename, 'r', err)
-
-    try:
+        mode = 'w'
         # Overwrite the old word list #
-        with open(filename, 'w') as report_file:
+        with open(filename, mode) as report_file:
             # Write result to report file #
             [report_file.write(f'{parse}\n') for parse in parse_set]
 
     # If file error occurs #
     except (IOError, OSError) as err:
-        ErrorQuery(filename, 'w', err)
+        PrintErr(f'Error occurred during wordlist sanitation: {err}')
+        ErrorQuery(filename, mode, err)
+        sys.exit(2)
 
     sys.exit(0)
 
